d69d52c0084499dda28e0d1da85e8818e831ee84
Kyle Coffey <me@phytolizer.dev>
2022-12-04 11:29:30 -0600

    remove redundant assignment

--- src/better_getline.c
+++ src/better_getline.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 
 bool better_getline(struct string* buffer, FILE* stream) {
-    buffer->length = 0;
-
     ssize_t len = getline(&buffer->data, &buffer->capacity, stream);
     if (len < 0) {
         return false;
